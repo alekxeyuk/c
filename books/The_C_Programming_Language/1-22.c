c002e914b8c21c2797f48d29b99d575a0211660f
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2025-04-07 18:32:44 +0300

    Create 1-22.c

---
+++ books/The_C_Programming_Language/1-22.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX_LINE (4 - 1)
+#define MAX_BUFF (MAX_LINE + 2)
+
+#define INWORD 1
+#define OUTWORD 0
+
+/*
+* A program to "fold" long input lines into two or more shorter lines
+* after the last non-blank character that occurs before the n-th column of input.
+*/
+
+static void dump_buff(char* buff, int n) {
+	for (int i = 0; i <= n; i++) {
+		putchar(buff[i]);
+	}
+}
+static int l_shift_buff(char* buff, int from) {
+	int count;
+	int ops = (MAX_BUFF - from);
+	for (count = 0; count < ops; count++) {
+		buff[count] = buff[from++];
+	}
+	return count;
+}
+
+int main(void) {
+	int ch;
+	int idx = 0;
+	int last_word_end = -1;
+	char state = OUTWORD;
+	char buff[MAX_BUFF] = { '\0' };
+
+
+	while ((ch = getchar()) != EOF) {
+		if (ch == '\n' && idx > 0) {
+			buff[idx] = '\0';
+			idx = 0;
+			last_word_end = -1;
+			state = OUTWORD;
+			printf("%s\n", buff);
+			continue;
+		}
+
+		buff[idx] = (char)ch;
+
+		if (state == INWORD && isspace(ch)) {
+			state = OUTWORD;
+			last_word_end = idx - 1;
+		}
+		else if (state == OUTWORD && !isspace(ch)) {
+			state = INWORD;
+		}
+
+		if (idx <= MAX_LINE) {
+			idx++;
+		}
+		else {
+			last_word_end = last_word_end >= 0 ? last_word_end : MAX_LINE;
+			dump_buff(buff, last_word_end);
+			idx = l_shift_buff(buff, last_word_end + 1);
+
+			last_word_end = -1;
+			putchar('\n');
+		}
+	}
+
+	return 0;
+}
