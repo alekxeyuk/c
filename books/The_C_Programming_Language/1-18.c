50dd3633f519bfd9fd3820340e75b0c5d25ae08d
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2025-04-25 11:09:17 +0300

    stupid getline

--- books/The_C_Programming_Language/1-18.c
+++ books/The_C_Programming_Language/1-18.c
@@ -5,7 +5,7 @@
 
 #define MAXLINE 100
 
-int getline(char line[], int maxline);
+int my_getline(char line[], int maxline);
 static int is_blank(int c);
 int trail_cleanup(char s[], int lim);
 
@@ -15,14 +15,14 @@ int main(void) {
 	char line[MAXLINE + 1];
 
 	putchar('>');
-	while ((len = getline(line, MAXLINE)) > 0) {
+	while ((len = my_getline(line, MAXLINE)) > 0) {
 		len = trail_cleanup(line, len > MAXLINE ? MAXLINE : len);
 		printf("%d  - |%s|\n>", len, line);
 	}
 	return 0;
 }
 
-int getline(char s[], int lim) {
+int my_getline(char s[], int lim) {
 	int c, i;
 
 	for (i = 0; (c = getchar()) != EOF and c != '\n'; i++)
