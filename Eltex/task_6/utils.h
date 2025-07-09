ee0e6cb984223a8a142645d7c68c67ac0557b24b
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2025-07-09 23:09:34 +0700

    Dumb error

--- Eltex/task_6/utils.h
+++ Eltex/task_6/utils.h
@@ -1,6 +1,12 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#ifdef _WIN32
+#define MY_SCANF(format, ...) scanf_s(format, __VA_ARGS__, (size_t)sizeof(__VA_ARGS__))
+#else
+#define MY_SCANF(format, ...) scanf(format, __VA_ARGS__)
+#endif
+
 int strncmp(const char*, const char*, size_t n);
 void flush_stdint(void);
 
