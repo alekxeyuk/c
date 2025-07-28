1bea03824e8ee808ccc861e6d7d4b00d22b9fb51
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2025-07-28 19:16:09 +0700

    Refactor panel rendering and file reading logic
    Extracted file reading, sorting, and panel rendering into separate functions for better modularity and readability. Improved color scheme handling, added top bar rendering with home directory abbreviation, and fixed panel selection logic. Removed redundant and commented-out code from main.

--- Eltex/task_9/part2/main.c
+++ Eltex/task_9/part2/main.c
@@ -11,15 +11,6 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
-// #include <fcntl.h>
-
-// #define STDIN_FILENO fileno(stdin)
-
-// void update_size(__attribute__((unused)) int signo) {
-//   struct winsize size;
-//   ioctl(STDIN_FILENO, TIOCGWINSZ, &size);
-//   resize_term(size.ws_row, size.ws_col);
-// }
 
 #define MTIMMX 12
 #define SMX 7
@@ -40,11 +31,20 @@ typedef struct PANEL {
   int cols;
 } PANEL;
 
+enum COLOR_SCHEME {
+  CS_DIR = 1,
+  CS_FILE,
+  CS_SELECTED,
+  CS_STATUS_ACT,
+  CS_STATUS_NACT
+};
+
 static void cleanup(void) { endwin(); }
 static bool prefix(const char* pre, const char* str) {
   return strncmp(pre, str, strlen(pre)) == 0;
 }
-static int compare_entries(const void* a, const void* b) {
+// Sorts FENTRY's into two categories: files and directories, with latter on top
+static int sort_by_type(const void* a, const void* b) {
   const FENTRY* entryA = (const FENTRY*)a;
   const FENTRY* entryB = (const FENTRY*)b;
 
@@ -63,24 +63,123 @@ void init_panel(PANEL* p, bool active, int cols, int y, int x) {
   p->nfiles = 0;
   p->active = active;
   p->win = newwin(LINES, cols, y, x);
-  p->cols = cols - 1;
+  p->cols = cols;
+  p->files = NULL;
+}
+// Scan files on path and populate panel FENTRY array, sorted
+bool read_folder(PANEL* a_pn, const char* path) {
+  struct stat sb;
+  struct dirent** namelist;
+  int list_size = scandir(path, &namelist, NULL, alphasort);
+  if (list_size == -1) {
+    return false;
+    /*perror("scandir");
+    exit(EXIT_FAILURE);*/
+  }
+
+  a_pn->nfiles = (size_t)list_size;
+  if (a_pn->files == NULL) {
+    a_pn->files = malloc(sizeof(FENTRY[list_size]));
+  } else {
+    a_pn->files = realloc(a_pn->files, sizeof(FENTRY[list_size]));
+  }
+  if (a_pn->files == NULL) {
+    return false;
+  }
+
+  for (int i = 0; i < list_size; i++) {
+    stat(namelist[i]->d_name, &sb);
+
+    strncpy(a_pn->files[i].name, namelist[i]->d_name,
+            sizeof(a_pn->files[i].name) - 1);
+    a_pn->files[i].name[sizeof(a_pn->files[i].name) - 1] = '\0';  // Save name
+    strftime(a_pn->files[i].mtime_str, sizeof(a_pn->files[i].mtime_str),
+             "%b %e %R", localtime(&sb.st_mtim.tv_sec));  // Save mtime
+    a_pn->files[i].is_dir = S_ISDIR(sb.st_mode);          // Save file type
+    a_pn->files[i].size = (intmax_t)sb.st_size;           // Save file size
+
+    free(namelist[i]);
+  }
+  free(namelist);
+
+  // Two category: file and directory
+  qsort(a_pn->files, a_pn->nfiles, sizeof(FENTRY), sort_by_type);
+  return true;
 }
 
-enum COLOR_SCHEME {
-  CS_DIR = 1,
-  CS_FILE,
-  CS_SELECTED,
-  CS_STATUS_ACT,
-  CS_STATUS_NACT
-};
+void draw_file_list(PANEL* a_pn) {
+  int color_scheme = CS_FILE;
+  for (size_t i = 0; i < a_pn->nfiles; i++) {
+    if (a_pn->files[i].is_dir) {
+      color_scheme = CS_DIR;
+    } else {
+      color_scheme = CS_FILE;
+    }
+    if (i == a_pn->selected && a_pn->active) {
+      color_scheme = CS_SELECTED;
+    }
+    wattron(a_pn->win, COLOR_PAIR(color_scheme));
+    mvwprintw(a_pn->win, (int)i + 1, 1, "%-*s", a_pn->cols - (MTIMMX + SMX + 3),
+              a_pn->files[i].name);
+    mvwprintw(a_pn->win, (int)i + 1, a_pn->cols - MTIMMX - SMX - 1, "%7jd",
+              a_pn->files[i].size);
+    mvwprintw(a_pn->win, (int)i + 1, a_pn->cols - MTIMMX, "%s",
+              a_pn->files[i].mtime_str);
+    wattroff(a_pn->win, COLOR_PAIR(color_scheme));
+  }
+  mvwvline(a_pn->win, 1, a_pn->cols - MTIMMX - SMX - 1 - 1, ACS_VLINE,
+           LINES - 2);
+  mvwvline(a_pn->win, 1, a_pn->cols - MTIMMX - 1, ACS_VLINE, LINES - 2);
+}
+// Static Getter/Setter for curent user's PWD
+const char* stored_user_pwd(void* ptr) {
+  static const char* pwd;
+  if (ptr != NULL) {
+    pwd = (const char*)ptr;
+  }
+  return pwd;
+}
+// Displays current path on top of the files list, replaces it with ~ if we are
+// at user's home dir
+void draw_top_bar(PANEL* a_pn) {
+  const char* user_pwd = stored_user_pwd(NULL);
+  const bool is_under_home = prefix(user_pwd, a_pn->cwd);
+  const int max_len = a_pn->cols - 8;
+  const int pwd_len = (int)strlen(user_pwd);
+  const int cwd_len = (int)strlen(a_pn->cwd);
+
+  int resulting_len = cwd_len;
+  if (is_under_home) {
+    resulting_len -= pwd_len;
+  }
+
+  const int color_scheme = a_pn->active ? CS_STATUS_ACT : CS_STATUS_NACT;
+  wattron(a_pn->win, COLOR_PAIR(color_scheme));
+  if (resulting_len > max_len) {
+    mvwprintw(a_pn->win, 0, 3, " ...%s ", a_pn->cwd + cwd_len - (max_len - 2));
+  } else if (is_under_home) {
+    mvwprintw(a_pn->win, 0, 3, " ~%s ", a_pn->cwd + pwd_len);
+  } else {
+    mvwprintw(a_pn->win, 0, 3, " %s ", a_pn->cwd);
+  }
+  wattroff(a_pn->win, COLOR_PAIR(color_scheme));
+}
+
+void render_panels(PANEL panels[2]) {
+  PANEL* a_pn;
+  for (int p_i = 0; p_i < 2; p_i++) {
+    a_pn = &panels[p_i];
+
+    a_pn->cols = (COLS + p_i) / 2 - 1;
+    box(a_pn->win, ACS_VLINE, ACS_HLINE);
+    draw_file_list(a_pn);
+    draw_top_bar(a_pn);
+    wnoutrefresh(a_pn->win);
+  }
+}
 
 int main(void) {
   setlocale(LC_ALL, "C.utf8");
-
-  // WINDOW* left;
-  // WINDOW* right;
-  // WINDOW* subwnd;
-  // signal(SIGWINCH, update_size);
 
   if (!initscr()) {
     perror("initscr failed!");
@@ -105,6 +204,7 @@ int main(void) {
 
   uid_t user_id = getuid();
   struct passwd* user_pwd = getpwuid(user_id);
+  stored_user_pwd(user_pwd->pw_dir);
 
   int selected = 0;
   int left_w = COLS / 2;
@@ -113,87 +213,18 @@ int main(void) {
   PANEL panels[2];
   init_panel(&panels[0], true, left_w, 0, 0);
   init_panel(&panels[1], false, right_w, 0, left_w);
-
-  PANEL* a_pn;
-
-  for (int p_i = 0; p_i < 2; p_i++) {
-    a_pn = &panels[p_i];
-
-    box(a_pn->win, ACS_VLINE, ACS_HLINE);
-
-    struct stat sb;
-    struct dirent** namelist;
-    int list_size = scandir(".", &namelist, NULL, alphasort);
-    if (list_size == -1) {
-      perror("scandir");
-      exit(EXIT_FAILURE);
-    }
-
-    a_pn->nfiles = (size_t)list_size;
-    a_pn->files = malloc(sizeof(FENTRY[list_size]));
-
-    for (int i = 0; i < list_size; i++) {
-      stat(namelist[i]->d_name, &sb);
-
-      strncpy(a_pn->files[i].name, namelist[i]->d_name,
-              sizeof(a_pn->files[i].name) - 1);
-      a_pn->files[i].name[sizeof(a_pn->files[i].name) - 1] = '\0';  // Save name
-      strftime(a_pn->files[i].mtime_str, sizeof(a_pn->files[i].mtime_str),
-               "%b %e %R", localtime(&sb.st_mtim.tv_sec));  // Save mtime
-      a_pn->files[i].is_dir = S_ISDIR(sb.st_mode);          // Save file type
-      a_pn->files[i].size = (intmax_t)sb.st_size;           // Save file size
-
-      free(namelist[i]);
-    }
-    free(namelist);
-    qsort(a_pn->files, a_pn->nfiles, sizeof(FENTRY), compare_entries);
-
-    int color_scheme = CS_FILE;
-    for (size_t i = 0; i < a_pn->nfiles; i++) {
-      if (a_pn->files[i].is_dir) {
-        color_scheme = CS_DIR;
-      } else {
-        color_scheme = CS_FILE;
-      }
-      if (i == a_pn->selected && a_pn->active) {
-        color_scheme = CS_SELECTED;
-      }
-      wattron(a_pn->win, COLOR_PAIR(color_scheme));
-      mvwprintw(a_pn->win, (int)i + 1, 1, "%-*s",
-                a_pn->cols - (MTIMMX + SMX + 3), a_pn->files[i].name);
-      mvwprintw(a_pn->win, (int)i + 1, a_pn->cols - MTIMMX - SMX - 1, "%7jd",
-                a_pn->files[i].size);
-      mvwprintw(a_pn->win, (int)i + 1, a_pn->cols - MTIMMX, "%s",
-                a_pn->files[i].mtime_str);
-      wattroff(a_pn->win, COLOR_PAIR(color_scheme));
-    }
-    mvwvline(a_pn->win, 1, a_pn->cols - MTIMMX - SMX - 1 - 1, ACS_VLINE,
-             LINES - 2);
-    mvwvline(a_pn->win, 1, a_pn->cols - MTIMMX - 1, ACS_VLINE, LINES - 2);
-
-    // TOP BAR CURRENT WORKING DIRECTORY
-    // getcwd(a_pn->cwd, sizeof(a_pn->cwd));  // TODO: can fail
-
-    color_scheme = a_pn->active ? CS_STATUS_ACT : CS_STATUS_NACT;
-    wattron(a_pn->win, COLOR_PAIR(color_scheme));
-    if (prefix(user_pwd->pw_dir, a_pn->cwd)) {
-      mvwprintw(a_pn->win, 0, 3, " ~%s ", a_pn->cwd + strlen(user_pwd->pw_dir));
-    } else {
-      mvwprintw(a_pn->win, 0, 3, " %s ", a_pn->cwd);
-    }
-    wattroff(a_pn->win, COLOR_PAIR(color_scheme));
-    // TOP BAR CURRENT WORKING DIRECTORY
-
-    // mvwaddstr(left, 2, 2, "Hello world");
-
-    wnoutrefresh(a_pn->win);
+  bool read_st = read_folder(&panels[0], panels[0].cwd);
+  read_st &= read_folder(&panels[1], panels[1].cwd);
+  if (!read_st) {
+    perror("Was not able to initialize panels with CWD of current user");
+    exit(EXIT_FAILURE);
   }
 
-  // wnoutrefresh(panels[1].win);
+  render_panels(panels);
   doupdate();
 
-  for (;;) {
-    int ch = wgetch(stdscr);
+  int ch;
+  while ((ch = wgetch(stdscr)) != 'q') {
     if (ch == KEY_RESIZE) {
       left_w = COLS / 2;
       right_w = COLS - left_w;
@@ -204,46 +235,20 @@ int main(void) {
       wresize(panels[0].win, LINES, left_w);
       wresize(panels[1].win, LINES, right_w);
       mvwin(panels[1].win, 0, left_w);
-
-      box(panels[0].win, ACS_VLINE, ACS_HLINE);
-      box(panels[1].win, ACS_VLINE, ACS_HLINE);
-
-      mvwprintw(panels[0].win, 1, 1, "New size: %d rows × %d cols", LINES,
-                COLS);
-      mvwprintw(panels[0].win, 3, 1, "Press 'q' to quit.");
-    } else if (ch == 'q' || ch == 'Q') {
-      break;
+      wnoutrefresh(stdscr);
     } else if (ch == KEY_UP) {
-      // selected++;
+      panels[selected].selected++;
     } else if (ch == KEY_DOWN) {
-      // selected = selected == 0 ? 0 : selected-1;
-    } else if (ch == KEY_STAB) {
+      panels[selected].selected--;
+    } else if (ch == '\t') {
+      panels[selected].active = false;
       selected = selected ? 0 : 1;
+      panels[selected].active = true;
     }
-    wnoutrefresh(stdscr);
-    wnoutrefresh(panels[0].win);
-    wnoutrefresh(panels[1].win);
+    render_panels(panels);
     doupdate();
   }
 
-  /*init_pair(1, COLOR_RED, COLOR_BLACK);
-  init_pair(2, COLOR_WHITE, COLOR_BLACK);
-  refresh();
-  wnd = newwin(32, 32, 2, 4);
-  box(wnd, '|', '-');
-  subwnd = derwin(wnd, 30, 30, 1, 1);
-  wattron(subwnd, A_BOLD | COLOR_PAIR(1));
-  wprintw(subwnd, "Hello brave new world!\n");
-  wattroff(subwnd, A_BOLD | COLOR_PAIR(1));
-  wrefresh(wnd);
-  delwin(subwnd);
-  delwin(wnd);
-
-  attron(A_BLINK | COLOR_PAIR(2));
-  move(9, 0);
-  printw("Press any button...");
-  refresh();
-  getch();*/
   free(panels[0].files);
   free(panels[1].files);
   delwin(panels[0].win);
