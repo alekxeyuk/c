6119d7fb36ce9985b38b01d372b4f78b03caa75d
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2025-07-29 23:24:04 +0700

    Create PopulateDir.ps1

---
+++ PopulateDir.ps1
@@ -0,0 +1,21 @@
+﻿foreach ($fileName in $args) {
+    Write-Host $fileName
+    $baseName = $fileName.Trim()
+
+    # Создаем .h файл
+    $headerFile = "$baseName.h"
+    $headerContent = @"
+#ifndef $($baseName.ToUpper())_H_
+#define $($baseName.ToUpper())_H_
+
+#endif // $($baseName.ToUpper())_H_
+"@
+    Set-Content -Path $headerFile -Value $headerContent
+
+    # Создаем .c файл с #include
+    $sourceFile = "$baseName.c"
+    $sourceContent = "#include `"$headerFile`""
+    Set-Content -Path $sourceFile -Value $sourceContent
+}
+
+Write-Host "Файлы .c и .h успешно созданы."
