c07ad3e33525a20eba9caab526e5f7bb57abe3bc
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2025-07-30 21:39:51 +0700

    Add calculator app with basic arithmetic operations
    Introduces a modular calculator application supporting addition, subtraction, multiplication, and division. Includes implementation and headers for each operation, a shared library interface, and a Makefile for building the project.

---
+++ Eltex/task_8/div.c
@@ -0,0 +1,8 @@
+#include "div.h"
+
+int divide(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    }
+    return 0;
+}
---
+++ Eltex/task_8/mul.h
@@ -0,0 +1,6 @@
+#ifndef MUL_H_
+#define MUL_H_
+
+int mul(int a, int b);
+
+#endif // MUL_H_
