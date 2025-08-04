f2c19680d2c8ba742eeac9157f3d4fe82782a616
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2025-08-04 22:52:13 +0700

    Task_15 part1
    Example of signals receiving/sending using sigaction and kill.

---
+++ Eltex/task_15/part1/producer.c
@@ -0,0 +1,59 @@
+#include <ctype.h>
+#include <errno.h>
+#include <locale.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static bool correct_pid_t(long v) {
+  pid_t p = (pid_t)v;
+  return (long)p == v;
+}
+
+static bool parse_pid(const char* s, pid_t* out) {
+  errno = 0;
+
+  while (isspace((unsigned char)*s)) s++;
+  if (*s == '\0') return false;
+
+  char* end = NULL;
+  long val = strtol(s, &end, 10);
+
+  if (errno == ERANGE || end == s || val < 0) return false;
+  if (!correct_pid_t(val)) return false;
+
+  *out = (pid_t)val;
+  return true;
+}
+
+int main(void) {
+  setlocale(LC_ALL, "C.utf8");
+
+  char buf[128];
+
+  printf("Please provide PID: ");
+
+  if (!fgets(buf, sizeof(buf), stdin)) {
+    perror("fgets failed");
+    exit(EXIT_FAILURE);
+  }
+
+  buf[strcspn(buf, "\n")] = '\0';
+
+  pid_t p;
+  if (!parse_pid(buf, &p)) {
+    fprintf(stderr, "invalid PID\n");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("I will send SIGUSR1 signals to the PID you provided\n");
+  while (true) {
+    kill(p, SIGUSR1);
+    usleep(10000);
+  }
+
+  exit(EXIT_SUCCESS);
+}
---
+++ Eltex/task_15/part1/receiver.c
@@ -0,0 +1,32 @@
+﻿#include <locale.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+static void action(int sig) {
+  printf("(%ld) Received signal = [%d]\n", time(NULL), sig);
+}
+
+int main(void) {
+  setlocale(LC_ALL, "C.utf8");
+
+  struct sigaction sa;
+  sa.sa_handler = action;
+  sigemptyset(&sa.sa_mask);
+  sigaddset(&sa.sa_mask, SIGUSR1);
+  sa.sa_flags = SA_RESTART;
+
+  if (sigaction(SIGUSR1, &sa, NULL) < 0) {
+    perror("sigaction failed");
+    exit(EXIT_FAILURE);
+  }
+
+  while (true) {
+    usleep(1000);
+  }
+
+  exit(EXIT_SUCCESS);
+}
